8ea920cef7dac0831b4141426f2b4c30a292fe41
Seun Joshua <sseun976@gmail.com>
2022-06-24 12:01:23 +0000

    sumbitSurvey

--- models/survey.models.js
+++ models/survey.models.js
@@ -1,21 +1,186 @@
-module.exports = mongoose =>{
-    var schema = mongoose.model('survey', 
-        mongoose.Schema({
-            name: String,
-            country: String,
-            email: String,
-            comment: String
-        },
-        {timestamp:true}
-        )
-    )
-    schema.method("toJSON", function(){
-        const {_v, _id, ...object} = this.toObject();
-        object.id = _id;
-        return object;
-    })
-    
-    const SurveyDetails = mongoose.model("survey", schema);
-    return SurveyDetails;
+const mongoose = require('mongoose');
+
+const rating= {
+    type:Number,
+min:1,
+max:5,
+required:true
 }
+// const validateEmail = (email) => {
+//     return String(email)
+//       .toLowerCase()
+//       .match(
+//         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//       );
+//   };
+
+const surveySchema = mongoose.Schema({
+    name: {
+        type: String,
+        minLength:3, 
+        maxLength:255,
+        required: true
+    },
+    country:{
+        type:String,
+        minLength:3,
+        maxLength:255,
+        required: true
+        
+    },
+    email:{
+        type:String,
+        Lowercase:true,
+        minLength:5,
+        maxLength:255,
+        // pattern:validateEmail,
+        required: true
+    },
+    rock1:{
+        'r1_1':rating,
+        'r1_2':rating,
+        'r1_3':rating,
+        'r1_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    rock2:{
+        'r2_1':rating,
+        'r2_2':rating,
+        'r2_3':rating,
+        'r2_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    rock3:{
+        'r3_1':rating,
+        'r3_2':rating,
+        'r3_3':rating,
+        'r3_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    pop1:{
+        'p1_1':rating,
+        'p1_2':rating,
+        'p1_3':rating,
+        'p1_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    pop2:{
+        'p2_1':rating,
+        'p2_2':rating,
+        'p2_3':rating,
+        'p2_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    pop3:{
+        'p3_1':rating,
+        'p3_2':rating,
+        'p3_3':rating,
+        'p3_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    jazz1:{
+        'j1_1':rating,
+        'j1_2':rating,
+        'j1_3':rating,
+        'j1_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    jazz2:{
+        'j2_1':rating,
+        'j2_2':rating,
+        'j2_3':rating,
+        'j2_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+       
+    },
+    jazz3:{
+        'j3_1':rating,
+        'j3_2':rating,
+        'j3_3':rating,
+        'j3_4':rating,
+        comment:{
+            type:String, 
+            minLength:3,
+            maxLength:500,
+            required: true 
+
+        }
+
+    },
+
+   
+},
+{timestamps:true}
+)
+const Survey = mongoose.model("survey", surveySchema);
+module.exports = Survey;
+
+
+// module.exports = mongoose =>{
+//     var schema = mongoose.model('survey', 
+        
+//     )
+//     // schema.method("toJSON", function(){
+//     //     const {_v, _id, ...object} = this.toObject();
+//     //     object.id = _id;
+//     //     return object;
+//     // })
+    
+//     return SurveyDetails;
+// }
 
--- server.js
+++ server.js
@@ -1,41 +1,297 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
+const Joi = require("joi");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+//this return a piece of middleware
+app.use(express.json());
 
-const db = require('./models')
+const db = require("./models");
 
-db.mongoose.connect(db.url, {
+db.mongoose
+  .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() =>{
+    useUnifiedTopology: true,
+  })
+  .then(() => {
     console.log("Connected to the database");
-})
-.catch(err =>{
+  })
+  .catch((err) => {
     console.log("Cannot connect to the database", err);
     process.exit();
-})
+  });
 
 var corsOptions = {
-    origin:"http://localhost:3000/"
-}
+  origin: "http://localhost:3000/",
+};
 
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.json({ message: "App running" });
+});
 
+app.get("/api/survey/:id", (req, res) => {
+  res.send([]);
+});
+app.post("/api/createSurvey/", (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string()
+      .min(3)
+      .required(),
+    country: Joi.string()
+      .min(3)
+      .required(),
+    email: Joi.string()
+      .min(3)
+      .required(),
+    rock1: {
+      r1_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
 
-app.get("/", (req, res)=>{
-    res.json({message:"App running"})
-})
+      r1_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
 
-app.listen({port: 8000}, () =>{
-    console.log('====================================');
-    console.log("Server is running on port 8000");
-    console.log('====================================');
-})
+      r1_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      r1_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    rock2: {
+      r2_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      r2_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      r2_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      r2_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    rock3: {
+      r3_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      r3_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      r3_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      r3_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    pop1: {
+      p1_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p1_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p1_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p1_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    pop2: {
+      p2_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p2_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p2_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p2_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    pop3: {
+      p3_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p3_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p3_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      p3_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    jazz1: {
+      j1_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j1_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j1_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j1_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    jazz2: {
+      j2_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j2_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j2_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j2_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+    jazz3: {
+      j3_1: Joi.number()
+        .integer()
+        .min(1)
+        .max(4),
+
+      j3_2: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j3_3: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+
+      j3_4: Joi.number()
+        .integer()
+        .min(1)
+        .max(5),
+      comment: Joi.string()
+        // .min(3)
+        .max(500),
+    },
+  });
+  const result = schema.validate(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const surveyRate = {
+    name: req.body.name,
+    country: req.body.country,
+    email: req.body.email,
+    rock1: req.body.rock1,
+    rock2: req.body.rock2,
+    rock3: req.body.rock3,
+    pop1: req.body.pop1,
+    pop2: req.body.pop2,
+    pop3: req.body.pop3,
+    jazz1: req.body.jazz1,
+    jazz2: req.body.jazz2,
+    jazz3: req.body.jazz3,
+  };
+  // surveyRate.push()
+  res.send(surveyRate);
+});
+
+app.listen({ port: 8000 }, () => {
+  console.log("====================================");
+  console.log("Server is running on port 8000");
+  console.log("====================================");
+});
